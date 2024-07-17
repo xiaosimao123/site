dd8ec2d4cfd4064c14f8fdabd46ed37b0d95c2de
xjun4882 <xjun4882@gmail.com>
2024-07-17 11:10:26 +0800

    chore: add cz

--- .cz-config.js
+++ .cz-config.js
@@ -1,60 +1,70 @@
 module.exports = {
-    // 可选类型
     types: [
-        {
-            value: ':sparkles: feat',
-            name: '✨ feat:      新功能'
-        },
-        {
-            value: ':bug: fix',
-            name: '  fix:      修复'
-        },
-        {
-            value: ':memo: docs',
-            name: '  docs:      文档变更'
-        },
-        {
-            value: ':lipstick: style',
-
-            name: '  style:     代码格式(不影响代码运行的变动)'
-        },
-        {
-            value: ':recycle: refactor',
-
-            name: '♻️  refactor:    重构 (既不增加feature, 也不是修复bug)'
-        },
-        {
-            value: ':zap: perf',
-            name: '⚡️ perf:      性能优化'
-        },
-        {
-            value: ':white_check_mark: test',
-            name: '✅ test:      增加测试'
-        },
-        {
-            value: ':wrench: chore',
-            name: '  chore:     构建过程或辅助工具的变动'
-        },
-        {
-            value: ':rewind: revert',
-            name: '⏪ revert:     回退'
-        },
-        {
-            value: ':rocket: build',
-            name: '  build:     打包'
-        }
+      { value: 'feat', name: 'feat:     A new feature' },
+      { value: 'fix', name: 'fix:      A bug fix' },
+      { value: 'docs', name: 'docs:     Documentation only changes' },
+      {
+        value: 'style',
+        name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+      },
+      {
+        value: 'refactor',
+        name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+      },
+      {
+        value: 'perf',
+        name: 'perf:     A code change that improves performance',
+      },
+      { value: 'test', name: 'test:     Adding missing tests' },
+      {
+        value: 'chore',
+        name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+      },
+      { value: 'revert', name: 'revert:   Revert to a commit' },
+      { value: 'WIP', name: 'WIP:      Work in progress' },
     ],
-
-    // 步骤
-
-    messages: {
-        type: '请选择提交的类型：',
-        customScope: '情输入修改的范围(可选)',
-        subject: '请简要描述提交(必填)',
-        body: '请输入详细描述(可选)',
-        footer: '请输入要关闭的issus(可选)',
-        confirmCommit: '确认要使用以上信息提交？(y/n)'
+  
+    scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
+  
+    usePreparedCommit: false, // to re-use commit from ./.git/COMMIT_EDITMSG
+    allowTicketNumber: false,
+    isTicketNumberRequired: false,
+    ticketNumberPrefix: 'TICKET-',
+    ticketNumberRegExp: '\\d{1,5}',
+  
+    // it needs to match the value for field type. Eg.: 'fix'
+    
+    scopeOverrides: {
+      fix: [
+  
+        {name: 'merge'},
+        {name: 'style'},
+        {name: 'e2eTest'},
+        {name: 'unitTest'}
+      ]
     },
-    // 默认长度72
-    subjectLimit: 72
-};
+   
+    // override the messages, defaults are as follows
+    messages: {
+      type: "Select the type of change that you're committing:",
+      scope: '\nDenote the SCOPE of this change (optional):',
+      // used if allowCustomScopes is true
+      customScope: 'Denote the SCOPE of this change:',
+      subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+      body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+      breaking: 'List any BREAKING CHANGES (optional):\n',
+      footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+      confirmCommit: 'Are you sure you want to proceed with the commit above?',
+    },
+  
+    allowCustomScopes: true,
+    allowBreakingChanges: ['feat', 'fix'],
+    // skip any questions you want
+    // skipQuestions: ['scope', 'body'],
+  
+    // limit subject length
+    subjectLimit: 100,
+    // breaklineChar: '|', // It is supported for fields body and footer.
+    // footerPrefix : 'ISSUES CLOSED:'
+    // askForBreakingChangeFirst : true, // default is false
+  };
